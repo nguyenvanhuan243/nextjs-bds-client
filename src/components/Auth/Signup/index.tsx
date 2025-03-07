ef0bb3247e62ffcdf823da2bf4b2387c5077c97a
Huan Nguyen <nguyenvanhuan243@gmail.com>
2025-03-07 22:20:12 +0700

    add create user

--- src/components/Auth/Signup/index.tsx
+++ src/components/Auth/Signup/index.tsx
@@ -1,8 +1,30 @@
+"use client";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
+  const [email, setEmail]       = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    axios.post("http://localhost:3001/api/create-new-user", {
+      email,
+      password,
+    }).then((res) => {
+      if (res.status === 201) {
+        alert("User created successfully");
+        window.location.replace("/signin");
+      } else {
+        alert("User creation failed");
+      }
+    }).catch((err) => {
+      console.log("err #############", err);
+      alert(err.response.data.message[0]);
+    });
+  };
+
   return (
     <>
       <Breadcrumb title={"Signup"} pages={["Signup"]} />
@@ -22,54 +44,56 @@ const Signup = () => {
             </span>
 
             <div className="mt-5.5">
-              <form>
+              <div className="mb-5">
+                <label htmlFor="email" className="block mb-2.5">
+                  Email Address <span className="text-red">*</span>
+                </label>
 
-                <div className="mb-5">
-                  <label htmlFor="email" className="block mb-2.5">
-                    Email Address <span className="text-red">*</span>
-                  </label>
+                <input
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                  }}
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email address"
+                  className="rounded-lg border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-3 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
+                />
+              </div>
 
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Enter your email address"
-                    className="rounded-lg border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-3 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-                  />
-                </div>
+              <div className="mb-5">
+                <label htmlFor="password" className="block mb-2.5">
+                  Password <span className="text-red">*</span>
+                </label>
+                <input
+                  onChange={(e) => {
+                    setPassword(e.target.value)
+                  }}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  autoComplete="on"
+                  className="rounded-lg border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-3 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
+                />
+              </div>
 
-                <div className="mb-5">
-                  <label htmlFor="password" className="block mb-2.5">
-                    Password <span className="text-red">*</span>
-                  </label>
+              <button
+                onClick={handleSignup}
+                className="w-full flex justify-center font-medium text-white bg-dark py-3 px-6 rounded-lg ease-out duration-200 hover:bg-blue mt-7.5"
+              >
+                Create Account
+              </button>
 
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Enter your password"
-                    autoComplete="on"
-                    className="rounded-lg border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-3 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-                  />
-                </div>
-
-                <button
-                  type="submit"
-                  className="w-full flex justify-center font-medium text-white bg-dark py-3 px-6 rounded-lg ease-out duration-200 hover:bg-blue mt-7.5"
+              <p className="text-center mt-6">
+                Already have an account?
+                <Link
+                  href="/signin"
+                  className="text-dark ease-out duration-200 hover:text-blue pl-2"
                 >
-                  Create Account
-                </button>
-
-                <p className="text-center mt-6">
-                  Already have an account?
-                  <Link
-                    href="/signin"
-                    className="text-dark ease-out duration-200 hover:text-blue pl-2"
-                  >
-                    Sign in Now
-                  </Link>
-                </p>
-              </form>
+                  Sign in Now
+                </Link>
+              </p>
             </div>
           </div>
         </div>
